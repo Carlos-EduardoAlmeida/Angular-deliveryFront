f87880590cc59f6f8ab0fe61aa1f32dc16d512e1
carlos_eduardo <ceduardoeab10@gmail.com>
2023-12-19 17:45:04 -0300

    fix: fixing loading problem

--- src/app/services/loading.service.ts
+++ src/app/services/loading.service.ts
@@ -1,30 +1,42 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, interval } from 'rxjs';
+import { BehaviorSubject, Observable, timer } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
   private loadingSubject = new BehaviorSubject<boolean>(false);
-  private firstTime = true;
+  private localStorageKey = 'firstTime';
+  private firstTime: boolean;
 
   loading$: Observable<boolean> = this.loadingSubject.asObservable();
 
+  constructor() {
+    this.firstTime = JSON.parse(localStorage.getItem(this.localStorageKey) || 'true');
+  }
+
   hide(): void {
     this.loadingSubject.next(false);
-    window.localStorage.setItem('showMessage', 'false')
+    window.localStorage.setItem('showMessage', 'false');
   }
-  
+
+  private executeOnceAfterDelay(): void {
+    timer(20000)
+      .pipe(take(1))
+      .subscribe(() => {
+        window.localStorage.setItem('showMessage', 'true');
+      });
+  }
+
   show(): void {
     window.localStorage.setItem('showMessage', 'false');
     this.loadingSubject.next(true);
-    if(this.firstTime == true){ 
-      const contador = interval(20000)
-      contador.subscribe({
-        next: () => {
-          window.localStorage.setItem('showMessage', 'true')
-        }
-      })
+
+    if (this.firstTime) {
+      this.executeOnceAfterDelay();
+      this.firstTime = false;
+      localStorage.setItem(this.localStorageKey, 'false');
     }
   }
 }
